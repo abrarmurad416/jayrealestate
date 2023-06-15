336a12d9cea5ee77d98239c312981ca4ba7d2959
Abrar <abrarmurad00@gmail.com>
2023-06-15 15:14:24 -0400

    image blob

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -32,7 +32,7 @@ export default function Landing() {
             </div>
             <div className="flex items-center justify-center lg:justify-end mx-10">
                 <img
-                    src="https://plus.unsplash.com/premium_photo-1661883982941-50af7720a6ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+                    src="/images/Group 1.png"
                     alt="Image"
                     className="w-full max-w-lg lg:max-w-none"
                 />
