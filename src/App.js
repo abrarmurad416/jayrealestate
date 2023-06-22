491917a0e131f8fb2c082ada5ed322413a5490fe
Abrar <abrarmurad00@gmail.com>
2023-06-22 14:51:41 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Header from "./components/Header/Header";
 import Landing from "./components/Landing/Landing";
 import Footer from "./components/Footer/Footer";
 import Reviews from "./components/Reviews/Reviews";
+import Contact from "./components/Contact/Contact";
 
 export default function App() {
     return (
         <div>
             <Header />
             <Landing />
-            <Reviews />
+            {/* <Reviews /> */}
+            <Contact />
             <Footer />
         </div>
     );
